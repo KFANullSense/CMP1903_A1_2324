3a73aabfe98b221b99a6768fd4de76b1c1c8d7b2
KFANullSense <nullsense@postcavus.com>
2024-03-06 22:35:31 +0000

    Final Tweak

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -5,7 +5,7 @@ namespace CMP1903_A1_2324
     internal class Die
     {
         //By making the Random instance static, it ensures that all rolls of the dice will continue along the same seed.
-        public static Random RNG = new Random();
+        public static Random Rng = new Random();
 
         //Provide a public way to get the current value of the dice. Set is private, as the dice will not need to be set outside of the roll function.
         public int CurrentValue { get; private set; }
@@ -13,7 +13,7 @@ namespace CMP1903_A1_2324
         //This method returns a random number between 1 and 6.
         public int Roll()
         {
-            int rolledValue = RNG.Next(1, 7); //Using the static Random instance, generate a random integer between 1 and 6.
+            int rolledValue = Rng.Next(1, 7); //Using the static Random instance, generate a random integer between 1 and 6.
 
             //Set the current value of the dice to the random value, and return it.
             CurrentValue = rolledValue;
